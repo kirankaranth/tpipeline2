66079079dd17069d9c86137c8f1372237f202993
kiran@simpledatalabs.com <kiran@simpledatalabs.com>
2022-10-28 09:24:13 +0000

    x

--- fdsfasf/pipelines/customers_orders/code/setup.py
+++ fdsfasf/pipelines/customers_orders/code/setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
+
 setup(
-    name = 'customers_orders',
-    version = '1.0',
-    packages = find_packages(include = ('job*', )) + ["prophecy_config_instances"],
-    package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
-    package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
-    description = 'workflow',
-    install_requires = [
-'prophecy-libs==1.3.8'],
-    entry_points = {
-'console_scripts' : [
-'main = job.pipeline:main'], },
-    data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
-    extras_require = {
-'test' : ['pytest', 'pytest-html', 'rich'], }
+    name='customers_orders',
+    version='1.0',
+    packages=find_packages(include=['job*']),
+    description='customers_orders',
+    install_requires=[
+        'prophecy-libs==1.3.8'
+    ],
+    entry_points={
+        'console_scripts': [
+            'main = job.pipeline:main',
+        ],
+    },
+    data_files=[(".prophecy", [".prophecy/workflow.latest.json"])],
+    extras_require={
+        'test': ['pytest', 'pytest-html'],
+    }
 )
