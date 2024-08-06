3524ea22f1148e2063fa08a9aa917b141e7be120
KiranKaranth <107342761+kirankaranth@users.noreply.github.com>
2024-08-06 08:07:28 +0530

    Update stg_payments.sql

--- HW_SQL_ext/models/staging/stg_payments.sql
+++ HW_SQL_ext/models/staging/stg_payments.sql
@@ -1,14 +1,9 @@
 WITH source AS (
-
   {#-
   Normally we would select from the table here, but we are using seeds to load
   our data in this project
   #}
-  SELECT * 
-  
-  FROM {{ ref('raw_payments')}}
-
-),
+  SELECT * FROM {{ ref('raw_payments')}}),
 
 renamed AS (
 
