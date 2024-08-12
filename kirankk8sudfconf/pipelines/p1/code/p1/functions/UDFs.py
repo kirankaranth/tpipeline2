73369efa5d83f1c92b4f92f8efe91e0a81335aaf
kiran@prophecy.io <kiran@prophecy.io>
2024-08-12 10:03:45 +0000

    Adds new pipeline 'p1' and UDF 'f1' in 'kirankk8sudfconf' repository.

---
+++ kirankk8sudfconf/pipelines/p1/code/p1/functions/UDFs.py
@@ -0,0 +1,29 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.lookups import (
+    createLookup,
+    createRangeLookup,
+    lookup,
+    lookup_last,
+    lookup_match,
+    lookup_count,
+    lookup_row,
+    lookup_row_reverse,
+    lookup_nth
+)
+
+def registerUDFs(spark: SparkSession):
+    spark.udf.register("f1", f1)
+    
+
+    try:
+        from prophecy.utils import ScalaUtil
+        ScalaUtil.initializeUDFs(spark)
+    except :
+        pass
+
+@udf(returnType = StringType())
+def f1(input):
+    # Your code here
+    return output
